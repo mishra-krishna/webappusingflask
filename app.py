d60930529a89c3317abefe8d378c06ec89a27a3a
Krishna <mishhra.krishhna@gmail.com>
2024-01-27 17:44:59 +0530

    second commit?

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from weather import get_weather_by_city as weather_main
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("API_KEY")
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    data = None
+    if request.method == 'POST':
+        city = request.form['cityInput']
+        state = request.form['stateInput']
+        country = request.form['countryInput']
+        print(f"Received form data: {city}, {state}, {country}")
+
+        data = weather_main(city, state, country, api_key)
+        print(f"Weather data: {data}")
+
+    return render_template('index.html',data=data)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- weather.py
+++ weather.py
@@ -2,28 +2,56 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+from dataclasses import dataclass
+
+@dataclass
+class WeatherData:
+    temp: str
+    icon: str
+    description: str
+    feels_like: str
+    humidity: str
+
+
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
-print(api_key)
+# print(api_key)
 
 
 def get_coordinates(city, state, country, api_key):
     response = requests.get(
-        f"https://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&appid={api_key}").json()
+        f"https://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&appid={api_key}&units=metric").json()
     lat = response[0]["lat"]
     lon = response[0]["lon"]
+    return lat, lon
 
-    print(lat, lon)
 
-
-get_coordinates("Lucknow", "UP", "India", api_key)
+# Get coordinates and store the values in lat and lon
+# lat, lon = get_coordinates("Lucknow", "UP", "India", api_key)
 
 def get_weather(lat, lon, api_key):
     response = requests.get(
-        f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={api_key}").json()
+        f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric").json()
 
-    print(response)
+    temp = 'Current temperature: '+str(response["main"]["temp"])+"°C"
+    icon = response["weather"][0]["icon"]
+    description = response["weather"][0]["description"]
+    feels_like = 'Feels Like: ' + str(response["main"]["feels_like"])+"°C"
+    humidity = "Humidity: " + str(response["main"]["humidity"])+"%"
+    data = WeatherData(temp, icon, description, feels_like, humidity)
+    # print(temp, icon, description, feels_like, humidity)
+    return data
 
+
+def get_weather_by_city(city, state, country, api_key):
+    lat, lon = get_coordinates(city, state, country, api_key)
+    weather_data = get_weather(lat, lon, api_key)
+    return weather_data
+
+
+if __name__ == '__main__':
+    lat, lon = get_coordinates("Lucknow", "UP", "India", api_key)
+    print(get_weather(lat, lon, api_key))
 
 
